941341f64d8862494d7590555d1351be7aff962d
Harshit Jain <55655148+Harshit1487@users.noreply.github.com>
2021-10-07 19:57:54 +0530

    Add components

---
+++ myComponents/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <div className="bg-dark text-light py-2 mb-0 my-4"style={{position: "relative",top:"15vh"}}>
+            <p className="text-center ">
+            copyright &copy; Text Analyzer
+            </p>
+        </div>
+    )
+}
